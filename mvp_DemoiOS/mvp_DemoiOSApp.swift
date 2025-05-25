7f5801966ba7c05d724f6288ed99ed6a80f82983
dezdoesit <dez.blair06@gmail.com>
2025-05-25 13:12:28 -0400

    Initial Commit

---
+++ mvp_DemoiOS/mvp_DemoiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  mvp_DemoiOSApp.swift
+//  mvp_DemoiOS
+//
+//  Created by Dezmond Blair on 5/25/25.
+//
+
+import SwiftUI
+
+@main
+struct mvp_DemoiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
